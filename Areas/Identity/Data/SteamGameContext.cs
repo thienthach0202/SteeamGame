ff5aaaf5e37b2f1577170d0b57d6434ccd948d93
HP <HP@DESKTOP-UA8ERMR>
2022-04-19 09:32:54 +0700

    Add project files.

---
+++ Areas/Identity/Data/SteamGameContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SteamGame.Areas.Identity.Data
+{
+    public class SteamGameContext
+    {
+    }
+}
---
+++ Data/SteamGameContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProGame.Models;
+
+namespace ProGame.Data
+{
+    public class ProGameContext : DbContext
+    {
+        public ProGameContext (DbContextOptions<ProGameContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProGame.Models.Game> Game { get; set; }
+    }
+}
